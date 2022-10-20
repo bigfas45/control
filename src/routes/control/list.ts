5d2c0a1d2ecff0520a13297dc231344128e08284
bigfas45 <ayodimejifasina@gmail.com>
2022-10-20 12:14:46 +0100

    udpate

--- src/routes/control/list.ts
+++ src/routes/control/list.ts
@@ -45,9 +45,9 @@ router.get('/api/control', async (req: Request, res: Response) => {
   // data["imports"]["@stanbic/sidebar"] =  "https://sbinternetbankingsidebar.web.app/main.js"
   data['imports']['@stanbic/sidebar'] =
     'https://sbinternetbankingsidebar.web.app/main.js';
-  data['imports']['@stanbic/header'] =
-    'https://sbinternetbankingheader.web.app/main.js';
-  // data["imports"]["@stanbic/header"] =  "http://localhost:9002/main.js"
+  // data['imports']['@stanbic/header'] =
+  //   'https://sbinternetbankingheader.web.app/main.js';
+  data["imports"]["@stanbic/header"] =  "http://localhost:9002/main.js"
   data['imports']['@stanbic/mobilemenu'] = 'http://localhost:9001/main.js';
 
   // console.log(data);
