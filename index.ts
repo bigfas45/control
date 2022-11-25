c703a4e5db562288ee42038a3e7ec2902cdcf7e0
bigfas45 <ayodimejifasina@gmail.com>
2022-11-25 13:14:47 +0100

    update

--- index.ts
+++ index.ts
@@ -1,24 +1,20 @@
 import mongoose from 'mongoose';
-
 import { app } from './app';
 
 const start = async () => {
   console.log('Starting.............. ..........');
 
   try {
-    // const uri = "mongodb+srv://admin:root@stanbic.m5eow.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-    const uri = 'mongodb://localhost:27017/control';
-
+    const uri =
+      'mongodb+srv://vbox:WguHoZGs8VRUY309@cluster0.zfbt1po.mongodb.net/?retryWrites=true&w=majority';
     await mongoose.connect(uri);
-
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
 
-  app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!!!');
+  app.listen(2000, () => {
+    console.log('Listening on port 2000!!!!!!!!!!');
   });
 };
 
